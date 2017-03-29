fb3956f2155bff4e3ae194480c35205738d3fb6c
Richard Lee <threadofethariadne@gmail.com>
2017-03-29 15:14:36 +0800

    Reworked web scraper as function

--- webScraper.py
+++ webScraper.py
@@ -1,27 +1,29 @@
 import urllib.request
 import re
 
-URL = 'http://www.a2zwordfinder.com/cgi-bin/scrabble.cgi'
-postDictionary = {
-    'SearchType': 'Scrabble',
-    'Pattern': '',
-    'MatchType': 'Anywhere',
-    'SubmitScrabble': 'Go',
-    'MinLetters' : '2',
-    'SortBy': 'Score'
-}
+def getHighestScoringWord(letters):
+    URL = 'http://www.a2zwordfinder.com/cgi-bin/scrabble.cgi'
+    postDictionary = {
+        'SearchType': 'Scrabble',
+        'Pattern': '',
+        'MatchType': 'Anywhere',
+        'SubmitScrabble': 'Go',
+        'MinLetters' : '2',
+        'SortBy': 'Score'
+    }
 
-# Letter input goes here
-postDictionary['Letters'] = 'rivienwasrauuanr'
+    # Letter input goes here
+    postDictionary['Letters'] = letters
 
-postRequest = urllib.parse.urlencode(postDictionary)
-req = urllib.request.Request(URL + '?' + postRequest)
-response = urllib.request.urlopen(req)
-rawPage = response.read().decode('utf-8')
-wordsFoundPattern = re.compile('(?<=<font face=Courier>\n)\w+?(?=</font>)')
-wordsPresent = wordsFoundPattern.search(rawPage)
-if wordsPresent is None:
-    print('No words found!')
-else:
-    print(wordsPresent.group(0))
-response.close()
+    postRequest = urllib.parse.urlencode(postDictionary)
+    req = urllib.request.Request(URL + '?' + postRequest)
+    response = urllib.request.urlopen(req)
+    rawPage = response.read().decode('utf-8')
+    #print(rawPage) # For debugging purposes
+    wordsFoundPattern = re.compile('(?<=<font face=Courier>\n)\w+?(?=(,|</font>))')
+    wordsPresent = wordsFoundPattern.search(rawPage)
+    if wordsPresent is None:
+        return False
+    else:
+        return(wordsPresent.group(0))
+    response.close()
