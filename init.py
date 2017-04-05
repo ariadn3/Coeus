5d131cc5337b2367233030149a656719b03ca489
Richard Lee <threadofethariadne@gmail.com>
2017-04-05 23:16:42 +0800

    Simple input for program

--- init.py
+++ init.py
@@ -1,10 +1,15 @@
 import webScraper as ws
 import screenSearch as ss
 import cv2
+import pyautogui
+import time
 
-
-img_rgb = cv2.imread('data/sampleScreenshots/ss4.png')
-output = ss.analyseScreenshot(img_rgb)
-print('{} = {}'.format(output[0], output[1]))
-
-# print(ws.getHighestScoringWord('burqa'))
+if __name__ == '__main__':
+    while True:
+        input('Press enter to begin analysis')
+        time.sleep(1)
+        im = pyautogui.screenshot('temp.png', region=(0, 0, 1002, 783))
+        img_rgb = cv2.imread('temp.png')
+        output = ss.analyseScreenshot(img_rgb)
+        print('{} = {}'.format(output[0], output[1]))
+        print(ws.getHighestScoringWord(output[0]))
